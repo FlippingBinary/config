a6f2e4330c5af95eb2050ec37d468442e3e7cbac
Jon Musselwhite <35066367+FlippingBinary@users.noreply.github.com>
2025-05-28 13:45:11 -0400

    feat: Test for KeePassXC before attempting password unlock

--- secrets.sh
+++ secrets.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env sh
 
+if ! command -v keepassxc-cli >/dev/null 2>&1; then
+  echo "Error: KeePassXC must be installed for this to work!" >&2
+  echo "Hint: Install it with 'sudo apt install keepassxc'." >&2
+  exit 1
+fi
+
 if [ ! -f "$KEEPASSXC_DB" ]; then
   echo "Error: KEEPASSXC_DB must be set to the path of the KeePassXC password database." >&2
   echo "Hint: Insert 'export KEEPASSXC_DB=\"/path/to/keepassxc.kdbx\" in \"$HOME/.profile\" or similar."
